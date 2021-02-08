ed375513a473ebcc93330a65ba808dfff6b60990
SuperMakerPlayer <51249300+SuperMakerPlayer@users.noreply.github.com>
2021-02-08 21:13:19 +0100

    fix echo sounds

--- characters/bullet/theme_reverb.gd
+++ characters/bullet/theme_reverb.gd
@@ -2,8 +2,13 @@ extends Node2D
 
 export(Array, NodePath) var exclude_nodes
 
+var echo_themes = [
+	1,
+	3
+]
+
 func _ready():
-	if CurrentLevelData.level.theme == 1:
+	if CurrentLevelData.level.theme in echo_themes:
 		for child in get_children():
 			if !(child.name in exclude_nodes):
 				child.set_bus("Reverb")
